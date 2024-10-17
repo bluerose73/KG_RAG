e06b380a90b4e8083c0cc0531c0140f9ba429068
yzjiao <yzjiao_ecnu@163.com>
2024-10-16 18:12:42 -0700

    Update config_loader.py

--- kg_rag/config_loader.py
+++ kg_rag/config_loader.py
@@ -7,8 +7,8 @@ with open('config.yaml', 'r') as f:
 with open('system_prompts.yaml', 'r') as f:
     system_prompts = yaml.safe_load(f)
     
-if 'GPT_CONFIG_FILE' in config_data:
-    config_data['GPT_CONFIG_FILE'] = config_data['GPT_CONFIG_FILE'].replace('$HOME', os.environ['HOME'])
+#if 'GPT_CONFIG_FILE' in config_data:
+#    config_data['GPT_CONFIG_FILE'] = config_data['GPT_CONFIG_FILE'].replace('$HOME', os.environ['HOME'])
 
     
 __all__ = [
